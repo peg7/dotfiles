645126c7e80b7c380e25993785d877e604da1b83
peg7 <108526093+peg7@users.noreply.github.com>
2022-10-10 16:24:36 +0900

    Add install.sh

---
+++ .bin/install.sh
@@ -0,0 +1,17 @@
+# original: https://bitbucket.org/durdn/cfg/src/master/.bin/install.sh
+REPO=https://github.com/peg7/dotfiles.git
+
+git clone --bare ${REPO} ${HOME}/.dotfiles
+function dotctl {
+   /usr/bin/git --git-dir=${HOME}/.dotfiles/ --work-tree=${HOME} $@
+}
+mkdir -p .dotfiles-backup
+dotctl checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles.";
+else
+  echo "Backing up pre-existing dotfiles.";
+  dotctl checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+dotctl checkout
+dotctl config status.showUntrackedFiles no
